cf944955c563e79d01176751c4b484d5958b32d5
I-AzraeL-I <qaraqan70@gmail.com>
2021-10-14 12:30:26 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/restreverse/RestReverseApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.restreverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestReverseApplication.class, args);
+    }
+
+}
