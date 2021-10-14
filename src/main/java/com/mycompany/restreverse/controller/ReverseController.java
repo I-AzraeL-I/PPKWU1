ffbe4fab5b09be519048d9d3ede1eeb7c6a8d494
I-AzraeL-I <qaraqan70@gmail.com>
2021-10-14 12:40:27 +0200

    Implement rest reverse method

--- src/main/java/com/mycompany/restreverse/controller/ReverseController.java
+++ src/main/java/com/mycompany/restreverse/controller/ReverseController.java
@@ -1,7 +1,16 @@
 package com.mycompany.restreverse.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReverseController {
+
+    @GetMapping("/reverse/")
+    public ResponseEntity<String> getReversedString(@RequestParam String inputData) {
+        var reversedData = new StringBuilder(inputData).reverse().toString();
+        return ResponseEntity.ok(reversedData);
+    }
 }
